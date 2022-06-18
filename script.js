056052373d95f066c3f68b08876e0e34c9814be7
khatiachub <101997849+khatiachub@users.noreply.github.com>
2022-06-19 03:12:42 +0400

    changed

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let burger=document.getElementById('burgerbar');
 let navigation=document.getElementById('burgernav');
 let overlay=document.getElementById('burgeroverlay');
+let xicon=document.getElementById('x-icon');
 burger.addEventListener('click', function(){
-    navigation.classList.add('active');
-    overlay.classList.add('active')
+    navigation.classList.add('activee');
+    overlay.classList.add('activee');
 })
 overlay.addEventListener('click', function(){
-    navigation.classList.remove('active');
-    overlay.classList.remove('active')
+    navigation.classList.remove('activee');
+    overlay.classList.remove('activee');
+})
+xicon.addEventListener('click', function(){
+    navigation.classList.remove('activee');
+    overlay.classList.remove('activee');
 })
